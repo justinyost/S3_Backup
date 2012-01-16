0104a4b594844fa2b1287b69e825ff1e7b1488ce
Justin Yost <justin.yost@yostivanich.com>
2012-01-15 18:13:14 -0600

    Updated Backup with ability to turn on/off database dumps
    Added ability to set MySQL/PostgreSQL/MongoDB dumps to run or not.
    
    Signed-off-by: Justin Yost <justin.yost@yostivanich.com>

--- backup.sh
+++ backup.sh
@@ -5,14 +5,26 @@ MYSQL_DB_PASSWORD=ThisIsYourMySQLRootPassword
 WEBROOT_LOCAL_PATH=/path/to/your/local/webroot
 BACKUP_LOCAL_PATH=/path/to/where/your/backups/will/be/locally
 AMAZON_S3_PATH=s3://BucketName/folder_in_bucket/
+MYSQL_DUMP_ACTIVE=TRUE
+POSTGRESQL_DUMP_ACTIVE=TRUE
+MONGODB_DUMP_ACTIVE=TRUE
 
 #DateTimeString
 THEDATE=`date +%Y_%m_%d_%H_%M`
 
 #Dump Databases for Daily DB Backups
-mysqldump --add-drop-table --all-databases --user=root --password=$MYSQL_DB_PASSWORD | gzip -9 > ${BACKUP_LOCAL_PATH}daily_db_backup_mysql_$THEDATE.sql.gz
-pg_dumpall | gzip -9 > ${BACKUP_LOCAL_PATH}daily_db_backup_postgresql_$THEDATE.sql.gz
-mongodump --host localhost | gzip -9 > ${BACKUP_LOCAL_PATH}daily_db_backup_mongodb_$THEDATE.sql.gz
+if [MYSQL_DUMP_ACTIVE == TRUE]
+then
+	mysqldump --add-drop-table --all-databases --user=root --password=$MYSQL_DB_PASSWORD | gzip -9 > ${BACKUP_LOCAL_PATH}daily_db_backup_mysql_$THEDATE.sql.gz
+fi
+if [POSTGRESQL_DUMP_ACTIVE == TRUE]
+then
+	pg_dumpall | gzip -9 > ${BACKUP_LOCAL_PATH}daily_db_backup_postgresql_$THEDATE.sql.gz
+fi
+if [MONGODB_DUMP_ACTIVE == TRUE]
+then
+	mongodump --host localhost | gzip -9 > ${BACKUP_LOCAL_PATH}daily_db_backup_mongodb_$THEDATE.sql.gz
+fi
 
 #Find and delete old daily database backups that are over a week old
 find ${BACKUP_LOCAL_PATH}daily_db_backup_* -mtime +7 -exec rm -f {} \;
@@ -27,9 +39,18 @@ find ${BACKUP_LOCAL_PATH}daily_site_backup_* -mtime +7 -exec rm -f {} \;
 if [ `date +%u` = 1 ]
 then
 	#Dump Databases for Weekly DB Backups
-	mysqldump --add-drop-table --all-databases --user=root --password=$MYSQL_DB_PASSWORD | gzip -9 > ${BACKUP_LOCAL_PATH}weekly_db_backup_mysql_$THEDATE.sql.gz
-	pg_dumpall | gzip -9 > ${BACKUP_LOCAL_PATH}weekly_db_backup_postgresql_$THEDATE.sql.gz
-	mongodump --host localhost | gzip -9 > ${BACKUP_LOCAL_PATH}weekly_db_backup_mongodb_$THEDATE.sql.gz
+	if [MYSQL_DUMP_ACTIVE == TRUE]
+	then
+		mysqldump --add-drop-table --all-databases --user=root --password=$MYSQL_DB_PASSWORD | gzip -9 > ${BACKUP_LOCAL_PATH}weekly_db_backup_mysql_$THEDATE.sql.gz
+	fi
+	if [POSTGRESQL_DUMP_ACTIVE == TRUE]
+	then
+		pg_dumpall | gzip -9 > ${BACKUP_LOCAL_PATH}weekly_db_backup_postgresql_$THEDATE.sql.gz
+	fi
+	if [MONGODB_DUMP_ACTIVE == TRUE]
+	then
+		mongodump --host localhost | gzip -9 > ${BACKUP_LOCAL_PATH}weekly_db_backup_mongodb_$THEDATE.sql.gz
+	fi
 	
 	#Find and delete old weekly database backups that are over 4 weeks old
 	find ${BACKUP_LOCAL_PATH}weekly_db_backup_* -mtime +28 -exec rm -f {} \;
@@ -45,9 +66,18 @@ fi
 if [ `date +%d` = 01 ]
 then
 	#Dump Databases for Monthly DB Backups
-	mysqldump --add-drop-table --all-databases --user=root --password=$MYSQL_DB_PASSWORD | gzip -9 > ${BACKUP_LOCAL_PATH}monthly_db_backup_mysql_$THEDATE.sql.gz
-	pg_dumpall | gzip -9 > ${BACKUP_LOCAL_PATH}monthly_db_backup_postgresql_$THEDATE.sql.gz
-	mongodump --host localhost | gzip -9 > ${BACKUP_LOCAL_PATH}monthly_db_backup_mongodb_$THEDATE.sql.gz
+	if [MYSQL_DUMP_ACTIVE == TRUE]
+	then
+		mysqldump --add-drop-table --all-databases --user=root --password=$MYSQL_DB_PASSWORD | gzip -9 > ${BACKUP_LOCAL_PATH}monthly_db_backup_mysql_$THEDATE.sql.gz
+	fi
+	if [POSTGRESQL_DUMP_ACTIVE == TRUE]
+	then
+		pg_dumpall | gzip -9 > ${BACKUP_LOCAL_PATH}monthly_db_backup_postgresql_$THEDATE.sql.gz
+	fi
+	if [MONGODB_DUMP_ACTIVE == TRUE]
+	then
+		mongodump --host localhost | gzip -9 > ${BACKUP_LOCAL_PATH}monthly_db_backup_mongodb_$THEDATE.sql.gz
+	fi
 	
 	#Find and delete old monthly database backups that are over 1 year old
 	find ${BACKUP_LOCAL_PATH}monthly_db_backup_* -mtime +356 -exec rm -f {} \;
