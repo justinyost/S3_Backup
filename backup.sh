0d2a5f34aa1145408e651fae864e3cad8426eca9
Justin Yost <justin.yost@yostivanich.com>
2012-02-15 07:57:21 +0000

    Converts backups from re-running database dumps and webroot tar/gzip to copying the latest daily backup to be the current weekly/monthly backup file. Fixes Issue #6

--- backup.sh
+++ backup.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-#Import settings from config.sh
-source ./config.sh
+#Import settings from ./config.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source ${DIR}/config.sh
 
 #DateTimeString
 THEDATE=`date +%Y_%m_%d_%H_%M`
@@ -24,7 +25,7 @@ fi
 find ${BACKUP_LOCAL_PATH}daily_db_backup_* -mtime +7 -exec rm -f {} \;
 
 #Tar and Gzip WWW Folder for Daily Backup
-tar -cf ${BACKUP_LOCAL_PATH}daily_site_backup_${THEDATE}.tar -X ./tar_exclude.txt ${WEBROOT_LOCAL_PATH}
+tar -cf ${BACKUP_LOCAL_PATH}daily_site_backup_${THEDATE}.tar -X ${DIR}/tar_exclude.txt ${WEBROOT_LOCAL_PATH}
 gzip -9 ${BACKUP_LOCAL_PATH}daily_site_backup_${THEDATE}.tar
 
 #Find and delete old daily site backups that are over a week old
@@ -32,26 +33,25 @@ find ${BACKUP_LOCAL_PATH}daily_site_backup_* -mtime +7 -exec rm -f {} \;
 
 if [ `date +%u` = 1 ]
 then
-	#Dump Databases for Weekly DB Backups
+	#Copy Daily Database Backup for Weekly DB Backups
 	if $MYSQL_DUMP_ACTIVE;
 	then
-		mysqldump --add-drop-table --all-databases --user=${MYSQL_DB_USERNAME} --password=$MYSQL_DB_PASSWORD | gzip -9 > ${BACKUP_LOCAL_PATH}weekly_db_backup_mysql_${THEDATE}.sql.gz
+		cp ${BACKUP_LOCAL_PATH}daily_db_backup_mysql_${THEDATE}.sql.gz ${BACKUP_LOCAL_PATH}weekly_db_backup_mysql_${THEDATE}.sql.gz
 	fi
 	if $POSTGRESQL_DUMP_ACTIVE;
 	then
-		pg_dumpall --username=${POSTGRES_DB_USERNAME} | gzip -9 > ${BACKUP_LOCAL_PATH}weekly_db_backup_postgresql_${THEDATE}.sql.gz
+		cp ${BACKUP_LOCAL_PATH}daily_db_backup_postgresql_${THEDATE}.sql.gz ${BACKUP_LOCAL_PATH}weekly_db_backup_postgresql_${THEDATE}.sql.gz
 	fi
 	if $MONGODB_DUMP_ACTIVE;
 	then
-		mongodump --host localhost --username ${MONGO_DB_USERNAME} --password ${MONGO_DB_PASSWORD} | gzip -9 > ${BACKUP_LOCAL_PATH}weekly_db_backup_mongodb_${THEDATE}.sql.gz
+		cp ${BACKUP_LOCAL_PATH}daily_db_backup_mongodb_${THEDATE}.sql.gz ${BACKUP_LOCAL_PATH}weekly_db_backup_mongodb_${THEDATE}.sql.gz
 	fi
 	
 	#Find and delete old weekly database backups that are over 4 weeks old
 	find ${BACKUP_LOCAL_PATH}weekly_db_backup_* -mtime +28 -exec rm -f {} \;
 	
-	#Tar and Gzip WWW Folder for Weekly Backup
-	tar -cf ${BACKUP_LOCAL_PATH}weekly_site_backup_${THEDATE}.tar -X ./tar_exclude.txt ${WEBROOT_LOCAL_PATH}
-	gzip -9 ${BACKUP_LOCAL_PATH}weekly_site_backup_${THEDATE}.tar
+	#Copy Daily Webroot Backup for Weekly Backup
+	cp ${BACKUP_LOCAL_PATH}daily_site_backup_${THEDATE}.tar.gz ${BACKUP_LOCAL_PATH}weekly_site_backup_${THEDATE}.tar.gz
 	
 	#Find and delete old daily site backups that are over 4 weeks old
 	find ${BACKUP_LOCAL_PATH}weekly_site_backup_* -mtime +28 -exec rm -f {} \;
@@ -59,26 +59,25 @@ fi
 
 if [ `date +%d` = 01 ]
 then
-	#Dump Databases for Monthly DB Backups
+	#Copy Daily Database Backup for Monthly DB Backups
 	if $MYSQL_DUMP_ACTIVE;
 	then
-		mysqldump --add-drop-table --all-databases --user=${MYSQL_DB_USERNAME} --password=$MYSQL_DB_PASSWORD | gzip -9 > ${BACKUP_LOCAL_PATH}monthly_db_backup_mysql_${THEDATE}.sql.gz
+		cp ${BACKUP_LOCAL_PATH}daily_db_backup_mysql_${THEDATE}.sql.gz ${BACKUP_LOCAL_PATH}monthly_db_backup_mysql_${THEDATE}.sql.gz
 	fi
-	if [${POSTGRESQL_DUMP_ACTIVE:=true} == TRUE]
+	if $POSTGRESQL_DUMP_ACTIVE;
 	then
-		pg_dumpall --username=${POSTGRES_DB_USERNAME} | gzip -9 > ${BACKUP_LOCAL_PATH}monthly_db_backup_postgresql_${THEDATE}.sql.gz
+		cp ${BACKUP_LOCAL_PATH}daily_db_backup_postgresql_${THEDATE}.sql.gz ${BACKUP_LOCAL_PATH}monthly_db_backup_postgresql_${THEDATE}.sql.gz
 	fi
 	if $MONGODB_DUMP_ACTIVE;
 	then
-		mongodump --host localhost --username ${MONGO_DB_USERNAME} --password ${MONGO_DB_PASSWORD} | gzip -9 > ${BACKUP_LOCAL_PATH}monthly_db_backup_mongodb_${THEDATE}.sql.gz
+		cp ${BACKUP_LOCAL_PATH}daily_db_backup_mongodb_${THEDATE}.sql.gz ${BACKUP_LOCAL_PATH}monthly_db_backup_mongodb_${THEDATE}.sql.gz
 	fi
 	
 	#Find and delete old monthly database backups that are over 1 year old
 	find ${BACKUP_LOCAL_PATH}monthly_db_backup_* -mtime +356 -exec rm -f {} \;
 	
-	#Tar and Gzip WWW Folder for monthly Backup
-	tar -cf ${BACKUP_LOCAL_PATH}monthly_site_backup_${THEDATE}.tar -X ./tar_exclude.txt ${WEBROOT_LOCAL_PATH}
-	gzip -9 ${BACKUP_LOCAL_PATH}monthly_site_backup_${THEDATE}.tar
+	#Copy Daily Webroot Backup for Monthly Backup
+	cp ${BACKUP_LOCAL_PATH}daily_site_backup_${THEDATE}.tar.gz ${BACKUP_LOCAL_PATH}monthly_site_backup_${THEDATE}.tar.gz
 	
 	#Find and delete old monthly site backups that are over 1 year old
 	find ${BACKUP_LOCAL_PATH}monthly_site_backup_* -mtime +356 -exec rm -f {} \;
@@ -87,4 +86,4 @@ fi
 #S3 Sync
 s3cmd sync --delete-removed ${BACKUP_LOCAL_PATH} $AMAZON_S3_PATH
 
-echo "S3Backup Successful: ${THEDATE}" >> ${BACKUP_LOCAL_PATH}s3backup.log
+echo "S3Backup Completed For: ${THEDATE}"
